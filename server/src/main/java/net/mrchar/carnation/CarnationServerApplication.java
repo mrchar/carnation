c84b2b3d0c95ab9925101c9a4ecd11c66da20188
mrchar <mrchar@qq.com>
2025-06-27 23:42:13 +0800

    创建server

---
+++ server/src/main/java/net/mrchar/carnation/CarnationServerApplication.java
@@ -0,0 +1,13 @@
+package net.mrchar.carnation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnationServerApplication.class, args);
+	}
+
+}
